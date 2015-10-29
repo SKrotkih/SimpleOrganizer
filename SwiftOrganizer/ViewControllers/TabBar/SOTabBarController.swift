009bb11a6f07aab8e7f538064a0ac7ef94d08ce7
Sergey Krotkih <sergey.krotkih@gmail.com>
2015-10-29 07:16:22 +0200

    commit to the task001 branch

--- SwiftOrganizer/ViewControllers/TabBar/SOTabBarController.swift
+++ SwiftOrganizer/ViewControllers/TabBar/SOTabBarController.swift
@@ -2,7 +2,7 @@
 //  SOTabBarController.swift
 //  SwiftOrganizer
 //
-//  Created by Sergey Krotkih on 5/31/15.
+//  Created by Sergey Krotkih on 10/29/15.
 //  Copyright (c) 2015 Sergey Krotkih. All rights reserved.
 //
 
